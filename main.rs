c00180251658267e6a429df9a1b5da3bfa3d48f1
Dan Cervo <dservo@users.noreply.github.com>
2019-08-17 16:18:43 -0700

    Registry edit working
    editing registry can now be done   key creation and deletetion  registry value  modify / create and delete    need to add logging and some of the other little features in the todo list
    
    todo:
    change file append string to somthing more meaningfull and to look more like a log entry (looking at this as fall back for last rec)
    send arguments to process from cli
    - get pid of new prosess
    receve data net trafic echo
    - get port info
    - get bytes sent
    logging

--- main.rs
+++ main.rs
@@ -3,121 +3,150 @@ extern crate clap;
 extern crate chrono;
 
 #[cfg(target_os = "windows")]
-    extern crate winreg;
+extern crate winreg;
 
 use chrono::{DateTime, Utc};
 use clap::App;
-use std::fs::{self , OpenOptions, File};
+use std::fs::{self, File, OpenOptions};
+use std::io::{BufRead, BufReader, Error, ErrorKind, Write};
+use std::net::TcpStream;
 use std::path::Path;
 use std::process::{Command, Stdio}; // self may be nedded
-use std::io::{self, BufRead, BufReader, Write, Error, ErrorKind}; // read notused at this time
-use std::net::TcpStream;
 
-// confused if #[cfg(target_os = "windows")] is single line only for this type of implenetation
-// but being safe atm
+// [NTS] remember can make use more compact
 #[cfg(target_os = "windows")]
-    use winreg::enums::*;
-#[cfg(target_os = "windows")]
-    use winreg::RegKey;
+use winreg::{enums::*, RegKey};
 
-fn main() -> std::io::Result<()>  {
-    // YAML Method for CLI OPTS
+fn main() -> std::io::Result<()> {
+    // YAML Method for CLI OPTS to have diffrent options
     // note the targeted compile fells wierd  but this works atm
+    // have to figure out a method to add the win file to nix to avoid double edit
     #[cfg(target_os = "windows")]
-        let yaml = load_yaml!("cli_win.yml");
+    let yaml = load_yaml!("cli_win.yml");
     #[cfg(not(target_os = "windows"))]
-         let yaml = load_yaml!("cli_nix.yml");
+    let yaml = load_yaml!("cli_nix.yml");
 
     let matches = App::from_yaml(yaml).get_matches();
     // get values for option supplied from command line or fall to default
     if matches.is_present("process") {
-      let process = matches.value_of("process").unwrap_or("default");
-      create_prosess(process.to_string())?;
+        let process = matches.value_of("process").unwrap_or("default");
+        create_prosess(process.to_string())?;
     }
     if matches.is_present("file") {
-      let file = matches.value_of("file").unwrap_or("default.txt");
-      if matches.is_present("create") {
-        create_file(file.to_string())?;
-      }else if matches.is_present("delete") {
-        remove_file(file.to_string())?;
-      }else if matches.is_present("modify") {
-        modify_file(file.to_string())?;
-      }else {
-        println!("Nothing Done about {}" , file.to_string());
-      }
+        let file = matches.value_of("file").unwrap_or("default.txt");
+        if matches.is_present("create") {
+            create_file(file.to_string())?;
+        } else if matches.is_present("delete") {
+            remove_file(file.to_string())?;
+        } else if matches.is_present("modify") {
+            modify_file(file.to_string())?;
+        } else {
+            println!("Nothing Done about {}", file.to_string());
+        }
     }
     //Collect the Net conection information
     if matches.is_present("net") {
-      let net: Vec<&str> = matches.values_of("net").unwrap().collect();
-      tcp_send(net[0].to_string(), net[1].to_string(), net[2].to_string())?;
+        let net: Vec<&str> = matches.values_of("net").unwrap().collect();
+        tcp_send(net[0].to_string(), net[1].to_string(), net[2].to_string())?;
     }
     if cfg!(target_os = "windows") {
-      if matches.is_present("reg") {
-        let reg_key = matches.value_of("reg").unwrap_or("default.txt");
-        if matches.is_present("create") {
-          win_reg_read()?;
-        }else if matches.is_present("delete") {
-        win_reg_read()?;
-        }else if matches.is_present("modify") {
-        win_reg_read()?;
-        }else {
-          println!("Nothing Done about {}" , reg_key.to_string());
+        if matches.is_present("reg") {
+            let reg_path = matches.value_of("reg").unwrap_or("Software\\default");
+            if matches.is_present("create") {
+                create_winreg_key(reg_path.to_string())?;
+            } else if matches.is_present("delete") {
+                if !matches.is_present("value") {
+                    remove_winreg_key(reg_path.to_string())?;
+                } else {
+                    // bit clunky on this overridden delete but scopeing needs some work
+                    let reg_value: Vec<&str> = matches.values_of("value").unwrap().collect();
+                    delete_winreg_value(reg_path.to_string(), reg_value[0].to_string())?;
+                }
+            } else if matches.is_present("modify") {
+                if matches.is_present("value") {
+                    let reg_value: Vec<&str> = matches.values_of("value").unwrap().collect();
+                    modify_winreg_value(
+                        reg_path.to_string(),
+                        reg_value[0].to_string(),
+                        reg_value[1].to_string(),
+                    )?;
+                } else {
+                    println!(
+                        "error: The argument --REGEDIT with flag -M requires -V
+                         <value>  and -N <name> to create the value"
+                    );
+                }
+            } else {
+                println!("Nothing Done about {}", reg_path.to_string());
+            }
         }
-      }
     }
     Ok(())
 } // end main
 
+fn check_file(path: String) -> bool {
+    Path::new(&path.clone()).exists()
+}
+
 fn create_file(path: String) -> std::io::Result<()> {
-    println!("Created file: {}", path);
-    let _file = File::create(path)?;
+    if check_file(path.clone()) {
+        println!("Created file: {}", path);
+        let _file = File::create(path)?;
+    } else {
+        println!("File alrady exsits");
+    }
     Ok(())
 }
 
 fn remove_file(path: String) -> std::io::Result<()> {
-    let file_check = Path::new(&path.clone()).exists();
-    if file_check == true {
-      println!("Deleted file: {}", path);
-      fs::remove_file(path)?;
+    if check_file(path.clone()) {
+        println!("Deleted file: {}", path);
+        fs::remove_file(path)?;
     } else {
-      println!("{} is not a file dooning nothing" , path);
+        println!("{} is not a file dooing nothing", path);
     }
     Ok(())
 }
 
 fn modify_file(path: String) -> std::io::Result<()> {
     let now: DateTime<Utc> = Utc::now();
-    let mut file_check = Path::new(&path.clone()).exists();
-    if file_check == false {
+    let mut file_check = check_file(path.clone()); //Path::new(&path.clone()).exists();
+    if !file_check {
         create_file(path.clone())?;
         file_check = true;
     }
-    if file_check == true {
+    if file_check {
         let mut _file = OpenOptions::new().append(true).open(path.clone()).unwrap();;
-        writeln!(_file,  "hello world")? ;
+        writeln!(_file, "hello world")?;
         println!("UTC now is: {}", now);
         println!("Modified file: {}", path);
     }
     Ok(())
 }
 
-fn create_prosess (path: String) -> std::io::Result<()> {
+fn create_prosess(path: String) -> std::io::Result<()> {
     println!("Running command: {}", path);
     let process_output = Command::new(path.clone())
         //.arg("--help") // todo:play with args() instead to pass from cli interface
         .stdout(Stdio::piped())
         .spawn()?
         .stdout
-        .ok_or_else(|| Error::new(ErrorKind::Other,"Error capturing standard output."))?;
+        .ok_or_else(|| Error::new(ErrorKind::Other, "Error capturing standard output."))?;
     let read_cmd_output = BufReader::new(process_output);
-    read_cmd_output.lines().filter_map(|line| line.ok()).for_each(|line| println!("{}", line));
+    read_cmd_output
+        .lines()
+        .filter_map(|line| line.ok())
+        .for_each(|line| println!("{}", line));
     Ok(())
 }
 
-fn tcp_send (ip_address: String, port: String, file: String) -> std::io::Result<()> {
-    println!("Connection Information IP: {} PORT: {} File: {} ", ip_address, port, file);
-    let connection = format!("{}:{}",ip_address , port);
-    if let Ok(mut tcp_stream) = TcpStream::connect (connection.clone()) {
+fn tcp_send(ip_address: String, port: String, file: String) -> std::io::Result<()> {
+    println!(
+        "Connection Information IP: {} PORT: {} File: {} ",
+        ip_address, port, file
+    );
+    let connection = format!("{}:{}", ip_address, port);
+    if let Ok(mut tcp_stream) = TcpStream::connect(connection.clone()) {
         println!("Connection to server @ : {}", connection);
         tcp_stream.write(b"hello world")?;
     } else {
@@ -126,60 +155,57 @@ fn tcp_send (ip_address: String, port: String, file: String) -> std::io::Result<
     Ok(())
 }
 
+// not implemneted yet, modify a key name appears to not be built into the winap
+// would need to read and create all values in a key and any sub keys in new key
+// then delet old key  [SHOULD ADD LATER]
+
 #[cfg(target_os = "windows")]
-fn win_reg_read() -> std::io::Result<()>{
+fn create_winreg_key(path: String) -> std::io::Result<()> {
+    // only going to use HKEY_CURRENT_USER atm for simpliity and premmisions
+    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
+    let reg_path = Path::new(&path);
+    let (_key, disp) = hkcu.create_subkey(&reg_path)?;
 
-    println!("Reading some system info...");
-      let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
-      let cur_ver = hklm.open_subkey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion")?;
-      let pf: String = cur_ver.get_value("ProgramFilesDir")?;
-      let dp: String = cur_ver.get_value("DevicePath")?;
-      println!("ProgramFiles = {}\nDevicePath = {}", pf, dp);
-      let info = cur_ver.query_info()?;
-      println!("info = {:?}", info);
-      let mt = info.get_last_write_time_system();
-      println!(
-          "last_write_time as winapi::um::minwinbase::SYSTEMTIME = {}-{:02}-{:02} {:02}:{:02}:{:02}",
-          mt.wYear, mt.wMonth, mt.wDay, mt.wHour, mt.wMinute, mt.wSecond
-      );
+    match disp {
+        REG_CREATED_NEW_KEY => println!("Created Key {}", &path),
+        REG_OPENED_EXISTING_KEY => println!("Key already exists {}", &path),
+    }
+    Ok(())
+}
 
-      // enable `chrono` feature on `winreg` to make this work
-      // println!(
-      //     "last_write_time as chrono::NaiveDateTime = {}",
-      //     info.get_last_write_time_chrono()
-      // );
+// study passing variable more to create a fn of open key
+// will break if key is not there but need to look into how to check for this condition
+// the panic! from the crate exsample does not work as gracefull as I would like
 
-      println!("And now lets write something...");
-      let hkcu = RegKey::predef(HKEY_CURRENT_USER);
-      let path = Path::new("Software").join("WinregRsExample1");
-      let (key, disp) = hkcu.create_subkey(&path)?;
+#[cfg(target_os = "windows")]
+fn remove_winreg_key(path: String) -> std::io::Result<()> {
+    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
+    let reg_path = Path::new(&path);
+    hkcu.open_subkey_with_flags(&reg_path, KEY_WRITE)?;
+    hkcu.delete_subkey_all(&reg_path)?;
+    Ok(())
+}
 
-      match disp {
-          REG_CREATED_NEW_KEY => println!("A new key has been created"),
-          REG_OPENED_EXISTING_KEY => println!("An existing key has been opened"),
-      }
+// Currently only working with REG_SZ
 
-      key.set_value("TestSZ", &"written by Rust")?;
-      let sz_val: String = key.get_value("TestSZ")?;
-      key.delete_value("TestSZ")?;
-      println!("TestSZ = {}", sz_val);
+#[cfg(target_os = "windows")]
+fn modify_winreg_value(path: String, name: String, value: String) -> std::io::Result<()> {
+    println!("test");
+    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
+    let reg_path = Path::new(&path);
+    let key = hkcu.open_subkey_with_flags(&reg_path, KEY_WRITE)?;
 
-      key.set_value("TestDWORD", &1234567890u32)?;
-      let dword_val: u32 = key.get_value("TestDWORD")?;
-      println!("TestDWORD = {}", dword_val);
+    key.set_value(name, &value)?;
+    Ok(())
+}
 
-      key.set_value("TestQWORD", &1234567891011121314u64)?;
-      let qword_val: u64 = key.get_value("TestQWORD")?;
-      println!("TestQWORD = {}", qword_val);
+#[cfg(target_os = "windows")]
+fn delete_winreg_value(path: String, name: String) -> std::io::Result<()> {
+    println!("test");
+    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
+    let reg_path = Path::new(&path);
+    let key = hkcu.open_subkey_with_flags(&reg_path, KEY_WRITE)?;
 
-      key.create_subkey("sub\\key")?;
-      hkcu.delete_subkey_all(&path)?;
-
-      println!("Trying to open nonexistent key...");
-      hkcu.open_subkey(&path).unwrap_or_else(|e| match e.kind() {
-          io::ErrorKind::NotFound => panic!("Key doesn't exist"),
-          io::ErrorKind::PermissionDenied => panic!("Access denied"),
-          _ => panic!("{:?}", e),
-      });
-      Ok(())
-} // end win_reg_read
+    key.delete_value(&name)?;
+    Ok(())
+}
